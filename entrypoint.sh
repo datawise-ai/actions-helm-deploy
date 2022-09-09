3c7f740302247c94db7f6152cb7f80dd73baaf20
Panos Georgantas <panos@georgantas.info>
2022-09-10 00:34:59 +0300

    first commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-env
+
+echo "$INPUT_KUBECONFIG_FILE" > .kubeconfig
+export KUBECONFIG=.kubeconfig
+
+if [ -n "$INPUT_CHART_REPO" ]; then
+	echo adding repo $INPUT_CHART_REPO_ALIAS $INPUT_CHART_REPO
+    helm repo add $INPUT_CHART_REPO_ALIAS $INPUT_CHART_REPO
+fi
+
+helm repo update
+
+echo helm upgrade --install --set deploy.project=$INPUT_PROJECT --set deploy.app=$INPUT_APP --set deploy.instance=$INPUT_INSTANCE --set deploy.version=$INPUT_VERSION -f $(echo $INPUT_VALUE_FILES | sed 's/,/ -f /g') $INPUT_RELEASE $CHART
+
+
+helm upgrade --install --set deploy.project=$INPUT_PROJECT --set deploy.app=$INPUT_APP --set deploy.instance=$INPUT_INSTANCE --set deploy.version=$INPUT_VERSION -f $(echo $INPUT_VALUE_FILES | sed 's/,/ -f /g') $INPUT_RELEASE $CHART
