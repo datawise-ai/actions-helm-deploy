eddc6cfce906a9de60be6af0c465f73a7701c7de
Panos Georgantas <panos@georgantas.info>
2022-09-16 14:06:29 +0300

    added instance val file

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,10 +12,18 @@ fi
 helm repo update
 helm search repo $INPUT_CHART_REPO_ALIAS
 
-echo helm upgrade --install --wait --timeout 2m0s --set deploy.project=$INPUT_PROJECT --set deploy.app=$INPUT_APP --set deploy.instance=$INPUT_INSTANCE --set deploy.service=$INPUT_SERVICE --set deploy.version=$INPUT_VERSION -f $(echo $INPUT_VALUE_FILES | sed 's/,/ -f /g') $INPUT_RELEASE $INPUT_CHART
+export VALFILE="$(echo $INPUT_VALUE_FILES | awk -F, '{print $(NF)}')"
+echo Searching for instance specific value file for $VALFILE
+export INSTANCE_VALFILE="$(dirname $VALFILE)/devel.$(basename $VALFILE)"
+if [ -f "$INSTANCE_VALFILE" ]; then
+	INCLUDE_VALFILE="-f $INSTANCE_VALFILE"
+	echo Found $INSTANCE_VALFILE :
+	cat $INSTANCE_VALFILE
+fi 
+
+echo helm upgrade --install --wait --timeout $INPUT_TIMEOUT --set deploy.project=$INPUT_PROJECT --set deploy.app=$INPUT_APP --set deploy.instance=$INPUT_INSTANCE --set deploy.service=$INPUT_SERVICE --set deploy.version=$INPUT_VERSION -f $(echo $INPUT_VALUE_FILES | sed 's/,/ -f /g') $INCLUDE_VALFILE $INPUT_RELEASE $INPUT_CHART
 
 
-helm upgrade --install --wait --timeout 2m0s --set deploy.project=$INPUT_PROJECT --set deploy.app=$INPUT_APP --set deploy.instance=$INPUT_INSTANCE --set deploy.service=$INPUT_SERVICE --set deploy.version=$INPUT_VERSION -f $(echo $INPUT_VALUE_FILES | sed 's/,/ -f /g') $INPUT_RELEASE $INPUT_CHART
-
+helm upgrade --install --wait --timeout $INPUT_TIMEOUT --set deploy.project=$INPUT_PROJECT --set deploy.app=$INPUT_APP --set deploy.instance=$INPUT_INSTANCE --set deploy.service=$INPUT_SERVICE --set deploy.version=$INPUT_VERSION -f $(echo $INPUT_VALUE_FILES | sed 's/,/ -f /g') $INCLUDE_VALFILE $INPUT_RELEASE $INPUT_CHART
 
 
